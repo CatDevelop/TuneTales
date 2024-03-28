f41da47e536f87a3db557476795ccae88cd0a8ac
Maxim Mineev <mms1401@ya.ru>
2024-03-28 23:00:53 +0500

    add eslint config

---
+++ Frontend/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AppComponent {
+    public title: string = 'TuneTales';
+}
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -5,16 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule
+    ],
+    providers: [
+        provideClientHydration()
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
