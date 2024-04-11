a55c3062505718653b7b6646a66224487f94db01
CatDeveloper <maks.rozhkov.2004@gmail.com>
2024-04-11 20:25:06 +0500

    feat: add taiga library

--- frontend/src/app/app.module.server.ts
+++ frontend/src/app/app.module.server.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
-import { ServerModule } from '@angular/platform-backend';
-
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
     imports: [
         AppModule,
-        ServerModule,
     ],
     bootstrap: [AppComponent],
 })
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TuiRootModule } from '@taiga-ui/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TuiButtonModule } from '@taiga-ui/core';
 
 @NgModule({
     declarations: [
         AppComponent
     ],
     imports: [
+        TuiButtonModule,
+        BrowserAnimationsModule,
+        TuiRootModule,
         BrowserModule,
         AppRoutingModule
     ],
